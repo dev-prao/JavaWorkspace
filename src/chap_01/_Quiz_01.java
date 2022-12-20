0fc6dd548ece9853082d042e21275e33450a24a1
prao <pch5521@gmail.com>
2022-12-20 17:58:43 +0900

    나도코딩 자바 강의 첫번째 날. 형변환까지 완료~!

---
+++ src/chap_01/_Quiz_01.java
@@ -0,0 +1,13 @@
+package chap_01;
+
+public class _Quiz_01 {
+    public static void main(String[] args) {
+        String busNumber = "상암08"; // String busNo
+        int remainingTime = 3; // int minute
+        float remainingDistance = 1.2f; // double distance
+
+        System.out.println(busNumber + "번 버스");
+        System.out.println("약 " + remainingTime + "분 후 도착");
+        System.out.println("남은 거리 " + remainingDistance + "km");
+    }
+}
