cae60c2806bdc157e3eb1f00f9d35f5487452aed
prao <pch5521@gmail.com>
2022-12-26 23:15:05 +0900

    나도코딩 클래스 ~ 메소드 오버로딩까지

---
+++ src/chap_07/BlackBox.java
@@ -0,0 +1,49 @@
+package chap_07;
+
+public class BlackBox {
+    String modelName;
+    String resolution;
+    int price;
+    String color;
+    static boolean canAutoReport = false; // 자동 신고 기능
+
+    void autoReport() {
+        if (canAutoReport) {
+            System.out.println("충돌이 감지되어 자동으로 신고합니다.");
+        } else {
+            System.out.println("자동 신고 기능이 지원되지 않습니다.");
+        }
+    }
+
+    void insertMemoryCard(int capacity) {
+        System.out.println("메모리 카드가 삽입되었습니다.");
+        System.out.println("용량은 " + capacity + "GB 입니다.");
+    }
+
+    int getVideoFileCount(int type) {
+        if (type == 1) { // 일반 영상
+            return 9;
+        } else if (type == 2) {
+            return 1;
+        }
+        return 10;
+    }
+
+    // showDataTime : 날짜정보 표시여부
+    // showSpeed : 속도정보 표시여부
+    // min : 영상 기록 단위(분)
+    void record(boolean showDateTime, boolean showSpeed, int min) {
+        System.out.println("녹화를 시작합니다.");
+        if (showDateTime) {
+            System.out.println("영상에 날짜정보가 표시됩니다.");
+        }
+        if (showSpeed) {
+            System.out.println("영상에 속도정보가 표시됩니다.");
+        }
+        System.out.println("영상은 " + min + "분 단위로 기록됩니다.");
+    }
+
+    void record() {
+        record(true, true, 5);
+    }
+}
