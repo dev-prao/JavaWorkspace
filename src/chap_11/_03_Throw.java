e9ac78e1ab9edd9e4096f6439702ae668c2573d3
prao <pch5521@gmail.com>
2023-01-19 22:20:56 +0900

    쓰레드 사용법

--- src/chap_11/_03_Throw.java
+++ src/chap_11/_03_Throw.java
@@ -2,7 +2,7 @@ package chap_11;
 
 public class _03_Throw {
     public static void main(String[] args) {
-        int age = 17; // 만 17세
+        int age = 19; // 만 17세
         try {
             if (age < 19) {
 //                System.out.println("만 19세 미만에게는 판매하지 않습니다.");
@@ -10,7 +10,7 @@ public class _03_Throw {
             } else {
                 System.out.println("주문하신 상품이 나왔습니다.");
             }
-            System.out.println(3 / 0);
+            System.out.println(3 / 1);
         } catch (Exception e) {
             e.printStackTrace();
         }
